47ca08244b44d1bc03617b82e49780de636e4ef8
liu.shiyu <shencangsheng@126.com>
2025-09-09 13:27:41 +0800

    feat: tsv

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::any::Any;
 use polars::prelude::PlPath::Local;
 use polars::prelude::{
     CsvReader, CsvWriterOptions, IntoLazy, LazyCsvReader, LazyFileListReader, LazyFrame, PlPath,
     SinkOptions, SinkTarget,
 };
 use polars::sql::SQLContext;
+use std::any::Any;
 
-use std::path::Path;
-use std::sync::Arc;
-use std::time::Instant;
+use crate::context::context::register_table;
 use sqlparser::ast::{Statement, TableFactor};
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
+use std::path::Path;
+use std::sync::Arc;
+use std::time::Instant;
 
 mod context;
 
 mod sql;
 
 fn main() {
-    // let mut context = SQLContext::new();
+    let mut context = SQLContext::new();
+
+    let sql = r#"
+            SELECT *
+            FROM read_tsv('/Users/shencangsheng/Downloads/nightly-VariantSummaries.tsv', infer_schema(false))"#;
+
+    let new_sql = register_table(&mut context, sql).unwrap();
+
+    println!("{}", new_sql);
+
+    let df = context
+        .execute(new_sql.as_str())
+        .unwrap()
+        .collect()
+        .unwrap();
+
+    df.iter().for_each(|row| {
+        println!("{:?}", row);
+    });
+
     //
     // let path: PlPath = Local(Arc::from(Path::new("/Users/shencangsheng/Code Repository/easy_db/example/order_*.csv")));
 
@@ -43,39 +63,37 @@ fn main() {
 
     // context.register("order", lf);
 
-
-
-    let start = Instant::now();
-
-    let sql = r#"
-            SELECT *
-            FROM read_tsv('/tmp/test.tsv', setnull(false))"#;
-
-    let mut parser = Parser::new(&GenericDialect);
-
-    let mut ast = parser
-        .try_with_sql(sql).unwrap().parse_statements().unwrap();
-
-    for stmt in &mut ast {
-        if let Statement::Query(query) = stmt {
-            if let sqlparser::ast::SetExpr::Select(select) = &mut *query.body {
-                for table_with_joins in &mut select.from {
-                    if let TableFactor::Table { name, args, .. } = &mut table_with_joins.relation {
-                        // 修改表名
-                        *name = sqlparser::ast::ObjectName(vec!["new_table_name".into()]);
-                    }
-                }
-            }
-        }
-    }
-
-    for stmt in ast {
-        if let Statement::Query(query) = stmt {
-            println!("{}", query.body.to_string());
-        }
-    }
-
-    println!("Parse time: {:?}", start.elapsed());
+    // let start = Instant::now();
+    //
+    // let sql = r#"
+    //         SELECT *
+    //         FROM read_tsv('/tmp/test.tsv', setnull(false))"#;
+    //
+    // let mut parser = Parser::new(&GenericDialect);
+    //
+    // let mut ast = parser
+    //     .try_with_sql(sql).unwrap().parse_statements().unwrap();
+    //
+    // for stmt in &mut ast {
+    //     if let Statement::Query(query) = stmt {
+    //         if let sqlparser::ast::SetExpr::Select(select) = &mut *query.body {
+    //             for table_with_joins in &mut select.from {
+    //                 if let TableFactor::Table { name, args, .. } = &mut table_with_joins.relation {
+    //                     // 修改表名
+    //                     *name = sqlparser::ast::ObjectName(vec!["new_table_name".into()]);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // for stmt in ast {
+    //     if let Statement::Query(query) = stmt {
+    //         println!("{}", query.body.to_string());
+    //     }
+    // }
+    //
+    // println!("Parse time: {:?}", start.elapsed());
 
     // let df_sql = SQLContext::new().execute(sql).unwrap().collect().unwrap();
     //
--- src-tauri/src/sql/parse.rs
+++ src-tauri/src/sql/parse.rs
@@ -19,7 +19,7 @@ pub fn parse_statements(sql: &str) -> EasyDBResult<Vec<Statement>> {
         .map_err(|e| e.into())
 }
 
-fn get_path(args: &mut Option<TableFunctionArgs>) -> EasyDBResult<PlPath> {
+pub fn get_path(args: &mut Option<TableFunctionArgs>) -> EasyDBResult<PlPath> {
     if args.is_none() {
         return Err(AppError::BadRequest {
             message: "The file path is missing.".to_string(),
@@ -45,88 +45,10 @@ fn get_path(args: &mut Option<TableFunctionArgs>) -> EasyDBResult<PlPath> {
     }
 }
 
-pub fn register_table(ctx: &mut SQLContext, sql: &str) -> EasyDBResult<String> {
-    let mut ast = parse_statements(sql)?;
 
-    let statement = ast.get_mut(0).ok_or(AppError::BadRequest {
-        message: "invalid SQL statement".to_string(),
-    })?;
-
-    let mut table_count = 1;
-
-    if let Statement::Query(query) = statement {
-        if let Select(select) = &mut *query.body {
-            for table_with_joins in &mut select.from {
-                if let TableFactor::Table { name, args, .. } = &mut table_with_joins.relation {
-                    let table_name = format!("t{}", table_count);
-                    let path = get_path(args)?;
-                    let lazy_frame: Option<LazyFrame> = match name.to_string().as_str() {
-                        "read_csv" => Some(read_csv(get_csv_reader(path), args)?),
-                        "read_tsv" => {
-                            let mut reader = get_csv_reader(path);
-                            reader = reader.with_separator(b'\t');
-                            Some(read_csv(reader, args)?)
-                        }
-                        _ => None,
-                    };
-
-                    if lazy_frame.is_none() {
-                        return Err(AppError::BadRequest {
-                            message: format!("'{}' is not a supported table function", table_name),
-                        });
-                    }
-
-                    *name = sqlparser::ast::ObjectName(vec![table_name.as_str().into()]);
-                    *args = None;
-                    ctx.register(table_name.as_str(), lazy_frame.unwrap());
-                }
-                table_count += 1;
-            }
-        }
-        return Ok("Query statement registered successfully".to_string());
-    } else {
-        return Err(AppError::BadRequest {
-            message: "Only supports Select statements.".to_string(),
-        });
-    }
-
-    Ok("".to_string())
-}
-
-fn get_function_args(args: &mut Option<TableFunctionArgs>) -> Option<&Vec<FunctionArg>> {
+pub fn get_function_args(args: &mut Option<TableFunctionArgs>) -> Option<&Vec<FunctionArg>> {
     if let Some(ref mut table_args) = args {
         return Some(&table_args.args);
     }
     None
-}
-
-fn get_csv_reader(path: PlPath) -> LazyCsvReader {
-    LazyCsvReader::new(path)
-}
-
-fn read_csv(
-    mut reader: LazyCsvReader,
-    args: &mut Option<TableFunctionArgs>,
-) -> EasyDBResult<LazyFrame> {
-    let args = get_function_args(args);
-
-    if let Some(args) = args {
-        for arg in args {
-            if let FunctionArg::Named { name, arg, .. } = arg {
-                match name.value.as_str() {
-                    "infer_schema" => {
-                        if let FunctionArgExpr::Expr(Expr::Value(Value::Boolean(value))) = arg {
-                            if (!value) {
-                                reader = reader.with_infer_schema_length(Some(0));
-                            }
-                        }
-                        break;
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-
-    reader.finish().map_err(|e| e.into())
 }
