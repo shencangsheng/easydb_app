8906d20fec954165457462dea3d2c572ced17572
liu.shiyu <shencangsheng@126.com>
2025-09-18 19:29:40 +0800

    fix: BUG

--- src-tauri/tests/collect_test.rs
+++ src-tauri/tests/collect_test.rs
@@ -1,9 +1,10 @@
 use app_lib::context::context::{collect, register};
+use calamine::{open_workbook, HeaderRow, Reader, Xlsx};
 use polars::sql::SQLContext;
+use std::error::Error;
 
 #[test]
 fn test_1() {
-
     let sql = "select * from read_excel('/Users/shencangsheng/Downloads/20250916111918.xlsx', sheet_name => '其他')";
 
     let mut context = SQLContext::new();
@@ -15,7 +16,6 @@ fn test_1() {
 
 #[test]
 fn test_2() {
-
     let sql = "select * from read_tsv('/Users/shencangsheng/Downloads/nightly-VariantSummaries.tsv', infer_schema => false)";
 
     let mut context = SQLContext::new();
